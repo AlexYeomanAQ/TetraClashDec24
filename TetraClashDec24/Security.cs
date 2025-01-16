9c6be33e0b4c1744a756daa148027a2c34eee63c
AlexYeomanAQ <aq233406@aquinas.ac.uk>
2025-01-16 16:01:54 +0000

    like changed one request

--- TetraClashDec24/Security.cs
+++ TetraClashDec24/Security.cs
@@ -9,18 +9,18 @@ namespace TetraClashDec24
 {
     class Security
     {
-        public static string GenerateSalt()
+        public static string GenerateSalt() //Random string
         {
             byte[] saltBytes = new byte[16];
             RandomNumberGenerator.Fill(saltBytes);
             return Convert.ToBase64String(saltBytes);
         }
 
-        public static string GenerateHash(string password, string salt)
+        public static string GenerateHash(string password, string salt) //passwordRandom string
         {
             string saltedPassword = password + salt;
             byte[] hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(saltedPassword));
-            return Convert.ToBase64String(hashBytes);
+            return Convert.ToBase64String(hashBytes); //hewdjslfwkeqoljfg
         }
     }
 }
